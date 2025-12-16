46078693894bf6dddef6d68e909d993bf7fa22d3
rafaelstarcevic <116400559+rafaelstarcevic@users.noreply.github.com>
2025-12-16 03:28:15 -0300

    Update criar_user.sh

--- criar_user.sh
+++ criar_user.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Criando usuarios do sistema...."
-useradd guest55 -c "Usuario Convidado" -s /bin/bash -m -p $(openssl passwd -crypt senha123)
+useradd guest55 -c "Usuario Convidado" -s /bin/bash -m -p $(openssl passwd senha123)
 passwd guest55 -e
-useradd guest44 -c "Usuario Convidado" -s /bin/bash -m -p $(openssl passwd -crypt senha123)
+useradd guest44 -c "Usuario Convidado" -s /bin/bash -m -p $(openssl passwd senha123)
 passwd guest44 -e
-useradd guest22 -c "Usuario Convidado" -s /bin/bash -m -p $(openssl passwd -crypt senha123)
+useradd guest22 -c "Usuario Convidado" -s /bin/bash -m -p $(openssl passwd senha123)
 passwd guest22
 echo "Finalizando!!"
